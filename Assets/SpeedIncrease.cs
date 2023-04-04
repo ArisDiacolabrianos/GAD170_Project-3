8295dbcdd6896fa030dbf341241c0c85fa49d440
SAE1\1016120 <diacolabrianos@gmail.com>
2023-04-04 17:57:31 +1000

    end of tuesday class

---
+++ Assets/SpeedIncrease.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedIncrease : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            collision.gameObject.GetComponent<PlayerMovement>().speed = 18f;
+        }
+    }
+
+    void OnTriggerStay(Collider collision)
+    {
+        
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            Debug.Log("hello");
+            collision.gameObject.GetComponent<PlayerMovement>().speed = 12f;
+        }
+    }
+}
